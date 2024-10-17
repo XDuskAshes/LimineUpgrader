7ef76bd469a57fc729e62fa4f9dd406d7039ba01
XDuskAshes <thegaymeradummy@gmail.com>
2024-10-16 21:23:28 -0400

    src/update-limine.sh - Ease of modification
    Made update-limine.sh easier to modify with just
    two extra variables.

--- src/update-limine.sh
+++ src/update-limine.sh
@@ -10,17 +10,16 @@
 
 LIMINE_INSTALL_PATH="/boot/EFI/BOOT" # Limine's installation path
 LIMINE_EFI_PATH="/usr/share/limine/BOOTX64.EFI" # Limine's UEFI file path
+PART_NAME="Arch Linux"
+DISK_BY_DEV="/dev/sda" # Replace this with the name of the *DISK* you have Limine on.
 
-cp -f "$LIMINE_EFI_PATH" "$LIMINE_INSTALL_PATH" # Forcefully replace (if not already there) BOOTX64.EFI in /boot/
+cp -f "$LIMINE_EFI_PATH" "$LIMINE_INSTALL_PATH/." # Forcefully replace (if not already there) BOOTX64.EFI in /boot/
 
 # gonna be honest this part is kinda slapped together from like four stack overflow articles
-# not 110% sure this works *properly* on all systems - please submit an issue if it doesn't
-#
-# i dont want to know why, i shouldnt have to wonder why, but this keeps making
-# the exact same boot entry names under different numbers each time - TODO: fix this
-efibootmgr | grep -q "Limine"
+
+efibootmgr | grep -q "$PART_NAME"
 if [ $? -ne 0 ]; then
-  efibootmgr --create --label "Limine" --disk /dev/sdX --part 1 --loader '\EFI\BOOT\BOOTX64.EFI'
+  efibootmgr --create --label "$PART_NAME" --disk $DISK_BY_DEV --part 1 --loader '\EFI\BOOT\BOOTX64.EFI'
 fi
 
 echo "If you experience issues after upgrading Limine, please check your UEFI settings and efibootmgr. Otherwise, submit an issue - <https://github.com/XDuskAshes/LimineUpgrader/>"
